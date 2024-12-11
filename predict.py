847d0b9b64611292e7cbcced907e40bb3e10260c
bavgg <gestopajonas@gmail.com>
2024-12-11 16:11:17 +0800

    ...

--- predict.py
+++ predict.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train/weights/best.pt")
 
-results = model.predict(source="data/images/test/iloveyou.1a9307e2-b436-11ef-bec6-d68e14d33a98.jpg", conf=0)
+results = model.predict(source="data/images/iamge.jpg", conf=0)
 print(results)
